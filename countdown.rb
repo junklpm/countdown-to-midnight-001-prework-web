343a5b9896f0d27b2d36176ec28303dbe4f46a2c
junklpm <lance.p.miller@gmail.com>
2016-07-12 21:29:08 -0400

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
-#write your code here
 
-def countdown
+
+def countdown(number)
+  number = 10
+while number < 11
+  puts "#{number} SECOND(S)!"
+  number -= 1
+  break if number == 0
 end
+
+  return "HAPPY NEW YEAR!"
+
+end
+
+def countdown_with_sleep(sleep)
+  sleep(5)
+  end
